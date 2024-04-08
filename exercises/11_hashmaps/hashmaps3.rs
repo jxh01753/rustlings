f9cca9e3dc1f970458a4889f6c87544c364d0ffa
John Harrington <jxha0@pm.me>
2024-04-08 20:09:00 +0100

    fixed the thing

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -15,8 +15,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -41,6 +39,7 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded by team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        //
 
         let team_1_entry = scores.entry(team_1_name).or_insert(Team {
             goals_scored: 0,
@@ -56,9 +55,7 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         });
 
         team_2_entry.goals_scored += team_2_score;
-        team_2_entry.goals_scored += team_1_score;
-
-        println!("{:?}", scores);
+        team_2_entry.goals_conceded += team_1_score;
     }
     scores
 }
